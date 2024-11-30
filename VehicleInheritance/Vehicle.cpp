f07ac0ec13a234b3d6f2179603e51b09a186d213
beelover715 <charles.schneider@sinclair.edu>
2024-11-29 20:39:20 -0500

    Vehicle class and methods created

--- VehicleInheritance/Vehicle.cpp
+++ VehicleInheritance/Vehicle.cpp
@@ -1,1 +1,56 @@
 #include "Vehicle.h"
+
+Vehicle::Vehicle()
+{
+	string manuNameHold;
+	int yearHold;
+
+	cout << "Vehicle:\n";
+	cout << "Enter the manufacturer: ";
+	while (!(getline(cin, manuNameHold)) || cin.fail()) {
+		cout << "\nEntry was invalid, please try again.\nEntry: ";
+		cin.clear();
+		cin.ignore();
+		cin.sync();
+	}
+	cout << "Enter the year built: ";
+	while (!(cin >> yearHold) || cin.fail()) {
+		cout << "\nEntry was NAN or invalid, please try again.\nEntry: ";
+		cin.clear();
+		cin.ignore();
+		cin.sync();
+	}
+}
+
+Vehicle::Vehicle(string manuName, int yearBuilt)
+{
+	Vehicle::manufacturer = manuName;
+	Vehicle::year = yearBuilt;
+}
+
+void Vehicle::setManufacturer(string getVehicleManu)
+{
+	Vehicle::manufacturer = getVehicleManu;
+}
+
+void Vehicle::setYear(int getVehicleYear)
+{
+	Vehicle::year = getVehicleYear;
+}
+
+string Vehicle::getManufacturer()
+{
+	return Vehicle::manufacturer;
+}
+
+int Vehicle::getYear()
+{
+	return Vehicle::year;
+}
+
+void Vehicle::displayInfo()
+{
+	cout << "Vehicle Information:\n";
+	cout << "Manufacturer: " << Vehicle::manufacturer << "\n";
+	cout << "Year Built: " << Vehicle::year << "\n";
+}
--- VehicleInheritance/Vehicle.h
+++ VehicleInheritance/Vehicle.h
@@ -1,9 +1,23 @@
 #pragma once
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
 class Vehicle
 {
 private:
-
+	string manufacturer;
+	int year;
 public:
 	Vehicle();
+	Vehicle(string, int);
+	void setManufacturer(string);
+	void setYear(int);
+	string getManufacturer();
+	int getYear();
+	virtual void displayInfo();
 };
 
